e11e827051b68caad74f6b35d575b9d90e269039
codesterkemp <sparky_csk@yahoo.com>
2013-06-11 20:49:14 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sixtoes::Application.config.secret_token = 'ca689e63acf2ed95ddf0f928f5bd94d0c38de3cd624fe563741bd0158e55df34e3e2de46802c1fe52a2aef9902f115e1f6078bbf4f8075cf287db1ff76a06993'
